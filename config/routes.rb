5af464e23a9ab1935403825125fb9e7daa90e92e
trndav <farsiteguide@gmail.com>
2023-11-13 12:18:39 +0100

    Added Comment model for articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   root "articles#index"
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
-  resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles do 
+    #nested resource
+    resources :comments 
+  end
 end
